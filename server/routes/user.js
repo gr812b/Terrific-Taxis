224126ce231a71acad9ef218331d6e69f47eef27
Andrew Thompson <thompa39@mcmaster.ca>
2023-04-11 11:22:49 -0400

    Seperate adding friends and removing friends into different endpoints

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signUp, signIn, editProfile, addFriend, getProfile, getOwnProfile } from '../controllers/userController.js';
+import { signUp, signIn, editProfile, addFriend, removeFriend, getProfile, getOwnProfile } from '../controllers/userController.js';
 import auth from '../middleware/auth.js';
 
 const router = express.Router();
@@ -8,6 +8,7 @@ router.post('/signin', signIn);
 router.post('/signup', signUp);
 router.patch('/', auth, editProfile);
 router.patch('/addfriend', auth, addFriend);
+router.patch('/removefriend', auth, removeFriend);
 router.get('/getprofile', auth, getProfile);
 router.get('/getownprofile', auth, getOwnProfile);
 
